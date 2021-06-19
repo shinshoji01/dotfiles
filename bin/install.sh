1ab3b5d65ceaadb948a251a7e8bffa59063dcf7a
Sho Inoue <shoshoinoue@icloud.com>
2021-06-19 20:02:03 +0900

    trial: delete fisher

--- bin/install.sh
+++ bin/install.sh
@@ -12,13 +12,13 @@ sudo apt -y install tmux
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt-get install -y fish
-exec fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+#exec fish
+# fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 
 ## fisher
-fisher install oh-my-fish/plugin-balias
-fisher install oh-my-fish/theme-bobthefish
-fisher install jethrokuan/z
-fisher install oh-my-fish/plugin-peco
-fisher install jethrokuan/fzf
-exec bash
+#fisher install oh-my-fish/plugin-balias
+#fisher install oh-my-fish/theme-bobthefish
+#fisher install jethrokuan/z
+#fisher install oh-my-fish/plugin-peco
+#fisher install jethrokuan/fzf
+#exec bash
