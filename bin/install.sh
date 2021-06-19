90dfe931c72b21011bdcec62a70e07f3c513dab6
Sho Inoue <shoshoinoue@icloud.com>
2021-06-19 19:31:26 +0900

    add fish

--- bin/install.sh
+++ bin/install.sh
@@ -4,3 +4,11 @@ pip3 install pep8
 
 # tmux
 sudo apt -y install tmux
+
+# fish
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:fish-shell/release-3
+sudo apt-get install -y fish
+fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+bash
