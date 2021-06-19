a8045dac56d17c6a91468a639283358a3981efbc
Sho Inoue <shoshoinoue@icloud.com>
2021-06-19 19:57:06 +0900

    trial: add fisher

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,9 @@
 pip3 install flake8
 pip3 install pep8
 
+# git
+sudo apt -y install git
+
 # tmux
 sudo apt -y install tmux
 
@@ -9,3 +12,13 @@ sudo apt -y install tmux
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt-get install -y fish
+exec fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+## fisher
+fisher install oh-my-fish/plugin-balias
+fisher install oh-my-fish/theme-bobthefish
+fisher install jethrokuan/z
+fisher install oh-my-fish/plugin-peco
+fisher install jethrokuan/fzf
+exec bash
