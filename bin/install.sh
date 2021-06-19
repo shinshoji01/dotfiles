25ebab1992924f05cc27eb972645e249934e6916
Sho Inoue <shoshoinoue@icloud.com>
2021-06-19 19:35:47 +0900

    delete fisher

--- bin/install.sh
+++ bin/install.sh
@@ -6,9 +6,6 @@ pip3 install pep8
 sudo apt -y install tmux
 
 # fish
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:fish-shell/release-3
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt-get install -y fish
-fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-bash
