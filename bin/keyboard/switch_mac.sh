3cf79cfda5807745d74c12ba17b2028e44aad66d
Sho Inoue <shoshoinoue@icloud.com>
2021-06-24 23:46:10 +0900

    add sh file for switching keyboard mode

---
+++ bin/keyboard/switch_mac.sh
@@ -0,0 +1,15 @@
+# installation of python revisers
+pip3 install flake8
+pip3 install pep8
+
+# installation of peco
+sudo apt  install peco
+
+# dotfiles' dir
+DOT_DIR="$HOME/Git/qmk_firmware"
+
+# git
+sudo apt -y install git
+
+# git clone
+git clone https://github.com/shinshoji01/qmk_firmware.git ${DOT_DIR}
