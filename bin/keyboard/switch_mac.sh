71995d256bb2ff6548548f10d8a9488f44f58803
Sho Inoue <shoshoinoue@icloud.com>
2021-06-25 00:04:59 +0900

    trial: switch_mac

--- bin/keyboard/switch_mac.sh
+++ bin/keyboard/switch_mac.sh
@@ -1,10 +1,3 @@
-# installation of python revisers
-pip3 install flake8
-pip3 install pep8
-
-# installation of peco
-sudo apt  install peco
-
 # dotfiles' dir
 DOT_DIR="$HOME/Git/qmk_firmware"
 
@@ -13,3 +6,7 @@ sudo apt -y install git
 
 # git clone
 git clone https://github.com/shinshoji01/qmk_firmware.git ${DOT_DIR}
+
+# qmk firmware
+cd ${DOT_DIR}
+qmk compile -kb 7skb -km sho-7skb 
