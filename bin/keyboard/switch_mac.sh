29d9d405c79bacc9a55b52b28f4ecae5f0ed5ed8
Sho Inoue <shoshoinoue@icloud.com>
2021-06-25 00:13:30 +0900

    Update switch_mac.sh

--- bin/keyboard/switch_mac.sh
+++ bin/keyboard/switch_mac.sh
@@ -1,14 +1,14 @@
-# dotfiles' dir
-DOT_DIR="$HOME/Git/qmk_firmware"
+# the directory of qmk_firmware
+QMK_DIR="$HOME/Git/qmk_firmware"
 
 # git
 sudo apt -y install git
 
 # git clone
-git clone https://github.com/shinshoji01/qmk_firmware.git ${DOT_DIR}
+git clone https://github.com/shinshoji01/qmk_firmware.git ${QMK_DIR}
 
 # qmk firmware
-cd ${DOT_DIR}
+cd ${QMK_DIR}
 git pull origin master
 qmk compile -kb 7skb -km sho-7skb 
 qmk flash -kb 7skb -km sho-7skb
