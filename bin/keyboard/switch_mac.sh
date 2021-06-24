cdf2cea68294c7ffd317033021d39bb14a59d7ba
Sho Inoue <shoshoinoue@icloud.com>
2021-06-25 01:12:23 +0900

    divide switch_mac 2

--- bin/keyboard/switch_mac.sh
+++ bin/keyboard/switch_mac.sh
@@ -1,17 +1,8 @@
 # the directory of qmk_firmware
 QMK_DIR="$HOME/Git/qmk_firmware"
 
-# git
-sudo apt -y install git
-
-# git clone
-git clone https://github.com/shinshoji01/qmk_firmware.git ${QMK_DIR}
-
 # qmk firmware
 cd ${QMK_DIR}
 git pull origin master
-./util/qmk_install.sh
-make git-submodule
-
-qmk compile -kb 7skb -km sho-7skb 
+qmk compile -kb 7skb -km sho-7skb
 qmk flash -kb 7skb -km sho-7skb
