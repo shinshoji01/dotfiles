c0b6b15cf2c6f7ffd0d5e8f2258f156586243eec
Sho Inoue <shoshoinoue@icloud.com>
2021-06-25 00:59:07 +0900

    Update switch_mac.sh

--- bin/keyboard/switch_mac.sh
+++ bin/keyboard/switch_mac.sh
@@ -10,5 +10,8 @@ git clone https://github.com/shinshoji01/qmk_firmware.git ${QMK_DIR}
 # qmk firmware
 cd ${QMK_DIR}
 git pull origin master
+./util/qmk_install.sh
+make git-submodule
+
 qmk compile -kb 7skb -km sho-7skb 
 qmk flash -kb 7skb -km sho-7skb
