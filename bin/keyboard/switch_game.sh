397559aa50599e2b3a1fdca9633aa0fad729df26
Sho Inoue <shoshoinoue@icloud.com>
2021-07-28 22:35:03 +0900

    add crkbd_game

---
+++ bin/keyboard/switch_game.sh
@@ -0,0 +1,8 @@
+# the directory of qmk_firmware
+QMK_DIR="$HOME/Git/qmk_firmware"
+
+# qmk firmware
+cd ${QMK_DIR}
+git pull origin master
+qmk compile -kb crkbd -km sho-crkbd_game
+qmk flash -kb crkbd -km sho-crkbd_game
